19913aa8fa63eea25b828474cafe8a05c17b023b
Ayaz Qureshi <ayazqqq03@gmail.com>
2020-12-13 00:41:23 +0530

    initial commit

---
+++ banana.js
@@ -0,0 +1,7 @@
+var txtInput = document.querySelector(#txt-input);
+var btnTranslate = document.querySelector(#btn-translate);
+
+clickEvenHandler(){
+    console.log('clicked')
+}
+btnTranslate.addEventListener('click', clickEvenHandler());
