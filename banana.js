7a5331719b0f43ffcb78999fd2c4e1c026861982
Ayaz Qureshi <ayazqqq03@gmail.com>
2020-12-13 21:31:35 +0530

    done

--- banana.js
+++ banana.js
@@ -2,9 +2,33 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-    outputDiv.innerHTML = "translated text" +txtInput.value;   
+ var link = "	https://api.funtranslations.com/translate/valyrian.json"
+ 
+function translationURL(text){
+    return link + "?" + "text=" + text
 }
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something went wrong");
+}
+
+function clickHandler() {
+    // outputDiv.innerHTML = "translated text " +txtInput.value;   
+    var inputText = txtInput.value;  //taking input
+
+    
+    // calling server for processing
+    fetch(translationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; //output
+    })
+    .catch(errorHandler)
+};
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
 
